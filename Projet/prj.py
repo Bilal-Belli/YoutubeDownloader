7a3e7d3b5a85e5346c0bdfacf419a408e66a591b
Bilal Belli <jb_belli@esi.dz>
2022-07-05 19:26:48 +0100

    creer le script pythone de telechargement youtube

--- Projet/prj.py
+++ Projet/prj.py
@@ -1,8 +1,3 @@
-from asyncio.constants import SSL_HANDSHAKE_TIMEOUT
-from distutils.command.sdist import sdist
-from ossaudiodev import SNDCTL_SEQ_CTRLRATE
-import ssl
-from this import s
 import pytube
 url = "https://www.youtube.com/watch?v=sc9NFJ-LrCk&list"#input('Enter here the Link: ')
 youtube = pytube.YouTube(url)
